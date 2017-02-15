3894b5790d8add83faaa84355645ecf5e09868fc
perttvil <vili.perttila@affecto.com>
2017-02-15 14:43:57 +0200

    added DI and services.

---
+++ src/app/mock-teams.ts
@@ -0,0 +1,19 @@
+import {Team} from "./team";
+
+export const TEAMS: Team[] = [
+  {id: 10, name: 'Tappara'},
+  {id: 11, name: 'Ilves'},
+  {id: 12, name: 'TPS'},
+  {id: 13, name: 'KalPa'},
+  {id: 14, name: 'JYP'},
+  {id: 15, name: 'HPK'},
+  {id: 16, name: 'Pelicans'},
+  {id: 17, name: 'Ässät'},
+  {id: 18, name: 'Kärpät'},
+  {id: 19, name: 'HIFK'},
+  {id: 20, name: 'Lukko'},
+  {id: 21, name: 'SaiPa'},
+  {id: 22, name: 'Sport'},
+  {id: 23, name: 'Kookoo'},
+  {id: 24, name: 'Jukurit'}
+];
---
+++ src/app/team.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {Team} from "./team";
+import {TEAMS} from './mock-teams';
+
+@Injectable()
+export class TeamService {
+  getTeams(): Promise<Team[]> {
+    return Promise.resolve(TEAMS);
+  }
+
+  getTeamsSlowly(): Promise<Team[]> {
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(this.getTeams()), 2000);
+    });
+  }
+}
